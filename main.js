a307cee29d2a96332f4e4bf16a5512259c31ec58
Rubcs <rubcastellon@gmail.com>
2023-01-30 15:31:12 +0100

    add some content to color-flipper

--- main.js
+++ main.js
@@ -9,29 +9,28 @@ const COLOR_PALETTE = {
 };
 
 const addOptionsToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
 
-const colorPickerSelect = document.querySelector('#color-picker');
+  Object.keys(COLOR_PALETTE).forEach((color) => {
+    const option = document.createElement("option");
+    option.value = color;
+    option.innerText = COLOR_PALETTE[color];
 
-Object.keys(COLOR_PALETTE).forEach((color) => {
-  const option = document.createElement('option');
-  option.value = color;
-  option.innerText = COLOR_PALETTE[color];
-
-  colorPickerSelect.append(option);
-});
+    colorPickerSelect.append(option);
+  });
 };
 
 const addEventListenerToColorPicker = () => {
-  const colorPickerSelect = document.querySelector('#color-picker');
-  const colorName = document.querySelector('#color-name');
+  const colorPickerSelect = document.querySelector("#color-picker");
+  const colorName = document.querySelector("#color-name");
 
-  colorPickerSelect.addEventListener('change', (ev) => {
+  colorPickerSelect.addEventListener("change", (ev) => {
     const newColor = ev.target.value;
 
     document.body.style.backgroundColor = newColor;
 
     const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`;
-    colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : '-';
+    colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
   });
 };
 
